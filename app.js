6cc3e8330e8225ba234559167134de463ac8a28a
asfand987 <asfand987.ak@gmail.com>
2020-06-18 15:59:40 +0100

    Added style.css

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
-var name = document.querySelector('.name');
-var desc = document.querySelector('.desc');
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
 var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
 
-button.addEventListener('click', function(){
-  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=d78785a6d83ea106897727876d2b3645').then(response => response.json()).then(data => console.log(data)) 
 
-.catch(err => alert("Wrong Name!"))
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=d78785a6d83ea106897727876d2b3645')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Wrong city name!"));
 })
